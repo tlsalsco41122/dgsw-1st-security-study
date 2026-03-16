4cf6d1721dba50732137e3e71035f92036ce9b2e
tlsalsco41122 <tlsalsco41122@gmail.com>
2026-03-16 15:44:53 +0900

    feat :: AuthApiController

--- src/main/java/com/kr/hs/dgsw_security/controller/AuthApiController.java
+++ src/main/java/com/kr/hs/dgsw_security/controller/AuthApiController.java
@@ -1,7 +1,21 @@
 package com.kr.hs.dgsw_security.controller;
 
+import com.kr.hs.dgsw_security.dto.SignupRequest;
+import com.kr.hs.dgsw_security.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // Controller + ResponseBody
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
 public class AuthApiController {
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public String signup(@RequestBody SignupRequest req) {
+
+    }
 }
