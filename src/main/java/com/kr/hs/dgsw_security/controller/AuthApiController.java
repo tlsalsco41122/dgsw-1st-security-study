2bf375ccd841b6ca78800eb3fcc69ab10ea15e88
tlsalsco41122 <tlsalsco41122@gmail.com>
2026-03-17 09:20:59 +0900

    feat :: signup api

--- src/main/java/com/kr/hs/dgsw_security/controller/AuthApiController.java
+++ src/main/java/com/kr/hs/dgsw_security/controller/AuthApiController.java
@@ -1,8 +1,11 @@
 package com.kr.hs.dgsw_security.controller;
 
+import com.kr.hs.dgsw_security.domain.User;
 import com.kr.hs.dgsw_security.dto.SignupRequest;
+import com.kr.hs.dgsw_security.dto.UserResponse;
 import com.kr.hs.dgsw_security.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthApiController {
     private final UserService userService;
+    /**
+     * 1. @RequestBody : body --> req --> json
+     * 2. @ModelAttribute : form 자바객체로 한번받음
+     * 3. @RequestParam : get(?a=3), form
+     * 4. @PathVariable : /user/1/aaa/2, url 경로에서 변수값 받음
+     *
+     * 5. @RequestHeader : header값 추출할 때
+     * 6. @CookieValue : cookie값 추출할 때
+     * 7. @RequestPart : 파일 + 데이터, multipart/form-data에서 파일 추출할 때
+     * 8. @HttpServletRequest : 옛날 거
+     */
 
     @PostMapping("/signup")
-    public String signup(@RequestBody SignupRequest req) {
+    public ResponseEntity<UserResponse> signup(@RequestBody SignupRequest req) {
+        User user = userService.signup(req);
+        UserResponse userResponse = new UserResponse(user.getId(), user.getEmail(), user.getRole());
 
+        return ResponseEntity.ok(userResponse);
     }
 }
