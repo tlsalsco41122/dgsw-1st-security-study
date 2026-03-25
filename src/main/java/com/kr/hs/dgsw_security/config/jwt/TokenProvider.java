aebddb08fcc60a1395e78d55c7debfc635d47dca
tlsalsco41122 <tlsalsco41122@gmail.com>
2026-03-25 10:44:58 +0900

    feat :: 토큰 생성, 검증, 클레입 정보 가져오기

--- src/main/java/com/kr/hs/dgsw_security/config/jwt/TokenProvider.java
+++ src/main/java/com/kr/hs/dgsw_security/config/jwt/TokenProvider.java
@@ -2,11 +2,15 @@ package com.kr.hs.dgsw_security.config.jwt;
 
 import com.kr.hs.dgsw_security.domain.User;
 import com.kr.hs.dgsw_security.repository.UserRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -25,7 +29,6 @@ public class TokenProvider {
         byte[] keyBytes = Decoders.BASE64.decode(jwtProperties.getSecretKey());
         this.secretKey = Keys.hmacShaKeyFor(keyBytes);
     }
-
     /*
      * Spring Bean lifecycle(도영왈)
      * 1. 스프링 컨테이너가 생성
@@ -37,14 +40,86 @@ public class TokenProvider {
      * 7. 스프링 종료
      */
 
-    public String createToken(User user, Duration duration) {
+    public String createToken(User user, Duration expireAt) {
         Date now = new Date();
-
+        return makeToken(
+                new Date(now.getTime() + expireAt.toMillis()),
+                user);
     }
 
     private String makeToken(Date expire, User user) {
         Date now = new Date();
         return Jwts.builder()
+                .header().type("JWT") // A
+                .and()
+                .issuer(jwtProperties.getIssuer()) // 토큰 발급자
+                .issuedAt(now) // 토큰 발급 시간
+                .expiration(expire) // 토큰 만료 시간
+                .subject(user.getEmail()) // 토큰 주체
+                .claim("id", user.getId()) // 토큰에 담을 정보, 클레임 추가
+                // B
+                .signWith(secretKey) // 서명 -> A + B + secretKey
+                .compact();
+    }
+    /*
+     * 클레임
+     * 비공개 - 내만 쓰는건가, 중복 신경 안써도됨.
+     * 공개 - 내가 LIB 를 공개, 중복 허용 안됨.
+     */
+
+    // 토큰 유효성 검증 메소드
+    public boolean validToken(String token) {
+        try {
+            Jwts.parser() // Jwts가 토큰 분석을 위해 파서를 준비
+                    .verifyWith(secretKey) // 서명을 분석할 키를 전달
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (SignatureException e) {
+            return false;
+        } catch (ExpiredJwtException e) {
+            return false;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    /*
+     * 예외
+     * 서명이 조작: SignatureException
+     * 토큰 만료: ExpiredJwtException
+     * 토큰 형식 오류: MalformedJwtException
+     * 토큰 값 비어있음: IllegalArgumentException
+     * 지원하지 않는 토큰: UnsupportedJwtException
+     */
+
+    // 클레임 정보(사용자 정보)를 가져오기 - payload
+    public Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    // subject 뽑아내기, 등록된 클레임 정보
+    public String getSubject(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    // 비공개(공개) 클레임
+    public Long getUserId(String token) {
+        return getClaims(token).get("id", Long.class);
+    }
+
+    /*Security 랑 연결하기
+     * 역할 : Jwt 에서 정보를 뽑아서 Authentication 의 출입증으로 변환
+     * Security 가 알아먹을 수 있는 객체로 변환해주는 역할
+     * 아는거
+     * - 사용자 정보 : UserDetails
+     * - 인증 : Authentication
+     */
+    public Authentication getAuthentication(String token) {
+        String email = getSubject(token); // 사용자 이메일
 
     }
 }
